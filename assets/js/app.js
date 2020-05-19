bf42ce3e53c9e8fd3b7cc4643ed4b8da288a3503
Tate Capra <tatecapra@outlook.com>
2020-05-19 18:17:38 -0500

    added basic calculator function to app file

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+
+//Building a Calculator
+// Steps 1: Create List of basic functions for Calculator
+// Step 2: Create Method for each operation in Calculator
+// Step 3: Test in Console
+
+function add(x, y){
+  return x + y;
+}
+
+function sub(x, y){
+  return x - y;
+}
+
+function mult(x, y){
+  return x * y;
+}
+
+
+function div(x, y){
+  return x / y;
+}
+// basic 2 input works
