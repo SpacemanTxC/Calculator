40a61bfaa397fb3198facc37947f4b923dcc4bc6
Tate Capra <tatecapra@outlook.com>
2020-05-19 20:22:16 -0500

    Created a basic four function calculator in the js assets direct through node

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
-
+// *********************************************************************
+//Notes and Practice
 //Building a Calculator
 // Steps 1: Create List of basic functions for Calculator
 // Step 2: Create Method for each operation in Calculator
 // Step 3: Test in Console
+
 
 // function add(x, y){
 //   return x + y;
@@ -42,21 +44,100 @@
 // console.log(`Hey there ${name}`);
 
 // Example 3 numbers
+// const prompt = require('prompt-sync')();
+//
+// const num1 = prompt('Enter a number: ');
+// const num2 = prompt('Enter a second number: ');
+// //convert to int from  string
+// function add(x, y){
+//   let number1 = parseInt(x);
+//   let number2 = parseInt(y);
+// // value
+//   return number1 + number2;
+//
+// };
+// //call and then show sum
+// console.log(add(num1, num2));
+//******************************************************************
+
+
+// Need to prompt what the user whats to do add, sub, mult, div
+// Terminal Calculator I created
+
+// npm package required
 const prompt = require('prompt-sync')();
 
-const num1 = prompt('Enter a number: ');
-const num2 = prompt('Enter a second number: ');
-//convert to int from  string
-function add(x, y){
-  let number1 = parseInt(x);
-  let number2 = parseInt(y);
-// value
-  return number1 + number2;
+// var to store the value of the first question asked
+const firstQ = prompt('Calculator: Enter add, sub for subtract, div for divide, or mult for multiple.  ');
 
+//Method to check what the user put in the terminal
+function checkResponce(Q1){
+
+// Lower case the value to be used in method
+  let question1 = Q1.toLowerCase();
+
+// if statement that checks the input and throws error
+    if(question1 == "add"){
+      // Prompt question to add
+        const firstAddQuestion = prompt('Enter first number to add :  ');
+        const secondAddQuestion = prompt('Enter second number to add :  ');
+      // add method
+          function add(x, y){
+            let number1 = parseInt(x);
+            let number2 = parseInt(y);
+
+            return number1 + number2;
+          };
+          // print value
+          console.log(add(firstAddQuestion, secondAddQuestion));
+
+    }else if(question1 == "sub"){
+      // Prompt question to subtract
+      const firstSubQuestion = prompt('Enter first number to subtract :  ');
+      const secondSubQuestion = prompt('Enter second number to subtract :  ');
+      // subtract method
+        function sub(x, y){
+          let number1 = parseInt(x);
+          let number2 = parseInt(y);
+
+          return number1 - number2;
+        };
+        // print value
+        console.log(sub(firstSubQuestion, secondSubQuestion));
+
+    }else if(question1 == "div"){
+      // Prompt question to divide
+      const firstDivQuestion = prompt('Enter first number to divide :  ');
+      const secondDivQuestion = prompt('Enter second number to divide :  ');
+      // divide method
+        function div(x, y){
+          let number1 = parseInt(x);
+          let number2 = parseInt(y);
+
+          return number1 / number2;
+        };
+        //print value
+        console.log(div(firstDivQuestion, secondDivQuestion));
+
+    }else if(question1 == "mult"){
+      // Prompt question to multiple
+      const firstMultQuestion = prompt('Enter first number to multiple :  ');
+      const secondMultQuestion = prompt('Enter second number to multiple :  ');
+      // multiple method
+        function mult(x, y){
+          let number1 = parseInt(x);
+          let number2 = parseInt(y);
+
+          return number1 * number2;
+        };
+        // print value
+        console.log(mult(firstMultQuestion, secondMultQuestion));
+
+    }else{
+      // throws error
+      console.log("Error: incorrect input try program again.  ");
+    };
 };
-//call and then show sum
-console.log(add(num1, num2));
 
-
-
-// Need to prompt what the user whats to do add, sub, mult, div 
+// calls method with value from first prompted question
+checkResponce(firstQ);
